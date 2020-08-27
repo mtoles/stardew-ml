6f48b5e50fae2ebef2c65f3e0768c1c655edefd1
Matthew Toles <toles.matthew@gmail.com>
2020-08-27 16:36:01 -0700

    watering relevancy matrix works

--- stardew-ilp.py
+++ stardew-ilp.py
@@ -5,11 +5,13 @@ import numpy as np
 e = 270 # starting energy
 g = 500 # starting gold
 
-b = np.array([30, 80])
-s = np.array([50, 175])
-f = np.array([4, 4])
+b = np.array([30, 80]) # seed/crop buy price
+s = np.array([50, 175]) # seed/crop sell price
+f = np.array([2, 2]) # planting energy
+w = np.array([2, 2]) # watering energy
+t = np.array([7, 12]) # growing time
 
-m = 4 # days in a season
+m = 28 # days in a season
 n = len(b) # number of different crops
 #foraging_arbitrage = np.zeros(len(b))
 #foraging_arbitrage[-1] = 1
@@ -20,16 +22,32 @@ energy_constraints = [] #f @ selection <= e
 pos_constraint = selection >= 0
 budget_constraints = []
 
-for i in range(m):
+# create budget constraints for each of m days
+for i in range(m): # i = today
 
     total_profits = cp.sum(selection[:i] @ (s - b))
     budget_constraints.append(
         selection[i] @ b <= total_profits + g
     )
 
-    energy_constraints.append(
-        cp.sum(selection[i]) * f <= e 
-    )
+    watering_budget = 0
+    watering_relevancy = np.zeros((m, n))
+    for k in range(m): # each day
+        for l in range(n): # each crop
+            if k <= i and k+t[l] >= i: # the day is before today but within the growing period
+                watering_relevancy[k,l] = 1
+    print(watering_relevancy)
+
+
+
+    # account energy for each crop
+    #for j in range(w):
+        #selection[:i][]
+        #watering_budget += selection[
+        #energy_constraints.append(
+            
+            #cp.sum(selection[i]) * f <= e 
+        #)
 
 constraints = budget_constraints + energy_constraints + [pos_constraint]
 profit = sum(selection @ (s-b))
