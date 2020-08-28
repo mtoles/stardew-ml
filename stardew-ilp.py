91436d9ec6f8ed6235478c1d0f49771b16694e3a
Matthew Toles <toles.matthew@gmail.com>
2020-08-27 17:04:01 -0700

    planting relevancy matrix works

--- stardew-ilp.py
+++ stardew-ilp.py
@@ -1,17 +1,19 @@
 import cvxpy as cp
 import numpy as np
+import pandas as pd
 # trees on average produce 58g worth of materials
 
 e = 270 # starting energy
 g = 500 # starting gold
 
+crop_names = np.array(["jazz", "cauliflower"])
 b = np.array([30, 80]) # seed/crop buy price
 s = np.array([50, 175]) # seed/crop sell price
 f = np.array([2, 2]) # planting energy
 w = np.array([2, 2]) # watering energy
 t = np.array([7, 12]) # growing time
 
-m = 28 # days in a season
+m = 20 # days in a season
 n = len(b) # number of different crops
 #foraging_arbitrage = np.zeros(len(b))
 #foraging_arbitrage[-1] = 1
@@ -30,13 +32,19 @@ for i in range(m): # i = today
         selection[i] @ b <= total_profits + g
     )
 
-    watering_budget = 0
     watering_relevancy = np.zeros((m, n))
+    planting_relevancy = np.zeros((m, n))
     for k in range(m): # each day
         for l in range(n): # each crop
             if k <= i and k+t[l] >= i: # the day is before today but within the growing period
                 watering_relevancy[k,l] = 1
-    print(watering_relevancy)
+            if k == i:
+                planting_relevancy[k,l] = 1
+    watering_costs = np.multiply(watering_relevancy, w)
+    planting_costs = np.multiply(planting_relevancy, f)
+    energy_constraints.append(
+        sum(cp.multiply(selection, watering_costs + planting_costs)) <= e
+    )
 
 
 
@@ -60,4 +68,16 @@ print(selection.value)
 #print("expenses: ", b @ selection.value)
 #print("revenue: ",  s @ selection.value)
 #print("profit: ", (s - b) @ selection.value)
+
+
+# Results
+df = pd.DataFrame(selection.value)
+df.columns = crop_names
+
+
+print(df)
+
+
+
+
 print()
