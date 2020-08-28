652185d1a0c4bbe0176f7346ffcd5f425921a890
Matthew Toles <toles.matthew@gmail.com>
2020-08-28 13:08:48 -0700

    new solver works. energy budget is k times what it should be where k is number of crops

--- stardew-ilp.py
+++ stardew-ilp.py
@@ -2,36 +2,36 @@ import cvxpy as cp
 import numpy as np
 import pandas as pd
 # trees on average produce 58g worth of materials
+import cvxopt.solvers as cvxopt
+cvxopt.options["maxiters"] = 1000
 
 e = 270 # starting energy
 g = 500 # starting gold
 
-crop_names = np.array(["jazz", "cauliflower"])
-b = np.array([30, 80]) # seed/crop buy price
-s = np.array([50, 175]) # seed/crop sell price
-f = np.array([2, 2]) # planting energy
-w = np.array([2, 2]) # watering energy
-t = np.array([7, 12]) # growing time
+crop_names = np.array(["jazz", "cauliflower", "garlic"])
+b = np.array([30, 80, 40]) # seed/crop buy price
+s = np.array([50, 175, 60]) # seed/crop sell price
+f = np.array([2, 2, 2]) # planting energy cost
+w = np.array([2, 2, 2]) # watering energy cost
+t = np.array([7, 12, 4]) # growing time
 
-m = 20 # days in a season
+m = 28 # days in a season
 n = len(b) # number of different crops
 #foraging_arbitrage = np.zeros(len(b))
 #foraging_arbitrage[-1] = 1
 
 selection = cp.Variable((m,n), integer=True)
 
-energy_constraints = [] #f @ selection <= e
+energy_demands = [] #f @ selection <= e
 pos_constraint = selection >= 0
 budget_constraints = []
 
 # create budget constraints for each of m days
 for i in range(m): # i = today
-
     total_profits = cp.sum(selection[:i] @ (s - b))
     budget_constraints.append(
         selection[i] @ b <= total_profits + g
     )
-
     watering_relevancy = np.zeros((m, n))
     planting_relevancy = np.zeros((m, n))
     for k in range(m): # each day
@@ -42,27 +42,16 @@ for i in range(m): # i = today
                 planting_relevancy[k,l] = 1
     watering_costs = np.multiply(watering_relevancy, w)
     planting_costs = np.multiply(planting_relevancy, f)
-    energy_constraints.append(
-        sum(cp.multiply(selection, watering_costs + planting_costs)) <= e
-    )
+    
+    energy_demands.append(sum(cp.multiply(selection, (watering_costs + planting_costs))))
+    energy_constraints = cp.hstack(energy_demands) <= e
 
-
-
-    # account energy for each crop
-    #for j in range(w):
-        #selection[:i][]
-        #watering_budget += selection[
-        #energy_constraints.append(
-            
-            #cp.sum(selection[i]) * f <= e 
-        #)
-
-constraints = budget_constraints + energy_constraints + [pos_constraint]
+constraints = budget_constraints + [energy_constraints] + [pos_constraint]
 profit = sum(selection @ (s-b))
 
 problem = cp.Problem(cp.Maximize(profit), constraints)
 
-problem.solve(solver=cp.GLPK_MI)
+problem.solve(solver=cp.CBC, verbose=True)
 
 print(selection.value)
 #print("expenses: ", b @ selection.value)
